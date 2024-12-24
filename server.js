ebdeb2d391567644b8811bca8a36e2d80aae6fdb
Wassim <wassim.besghayer@gmail.com>
2024-12-24 03:09:23 +0100

    setting up API

---
+++ server.js
@@ -0,0 +1,39 @@
+const API_key = "b12624dd5e1945192f57ac1b41582e9e"
+const API_URL= "https://api.openweathermap.org/data/2.5/weather?q=gabes&appid=b12624dd5e1945192f57ac1b41582e9e&units=metric"
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(API_URL + city + `&appid = ${API_key}`);
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"; 
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds") {
+        weatherIcon.src = "https://www.svgrepo.com/show/276635/cloudy-cloud.svg"
+    }
+    else if(data.weather[0].main == "Rain") {
+        weatherIcon.src = "https://www.svgrepo.com/show/279144/rain-forecast.svg"
+    }
+    else if(data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "https://www.svgrepo.com/show/22289/drizzle.svg"
+    }
+    else if(data.weather[0].main == "Snow") {
+        weatherIcon.src = "https://www.svgrepo.com/show/396043/cloud-with-snow.svg"
+    }
+    else if(data.weather[0].main == "Mist") {
+        weatherIcon.src = "https://www.svgrepo.com/show/491949/weather-color-sun-cloud.svg"
+    }
+    else (weatherIcon.src = "https://www.svgrepo.com/show/407540/sun.svg")  // clear weather → svg = sun
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+checkWeather(city);
